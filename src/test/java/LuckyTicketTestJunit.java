816d21ee0b0bdd4c0a9d400f71dd725b20e96dc9
Taras-Mila <wildmonach@gmail.com>
2022-07-16 23:35:23 +0300

    Everything is done

--- src/test/java/LuckyTicketTestJunit.java
+++ src/test/java/LuckyTicketTestJunit.java
@@ -1,5 +1,4 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import taras.LuckyTicket;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,11 +10,13 @@ public class LuckyTicketTestJunit {
 
     @Test
     public void shouldCreateDefaultArrayTest() {
+        System.out.println("TEST1");
         assertEquals(arrayLuckyTicket.toString(), "[3, 8, 3, 3, 3, 8]");
     }
 
     @Test
     void verifyArrayEqualIntegerValue() {
+        System.out.println("TEST2");
         int[] array = {3, 8, 3, 3, 3, 8};
         int[] actualResult = LuckyTicket.resultArray(array);
 
@@ -23,4 +24,23 @@ public class LuckyTicketTestJunit {
         Assertions.assertArrayEquals(expectedResult, actualResult);
     }
 
+    @BeforeEach
+    public void beforeTest() {
+        System.out.println("This is text before each test");
+    }
+
+    @AfterEach
+    public void afterTest(){
+        System.out.println("This is text after each test");
+    }
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Before all tests …");
+    }
+
+    @AfterAll
+    public static void afterAll(){
+        System.out.println("After all tests …");
+    }
 }
--- src/test/java/LuckyTicketTestNg.java
+++ src/test/java/LuckyTicketTestNg.java
@@ -1,6 +1,6 @@
+import org.testng.annotations.*;
 import taras.LuckyTicket;
 import org.testng.Assert;
-import org.testng.annotations.Test;
 
 //проверяем, что выводит метод toString() класса LuckyTicket.
 // Метод Assert.assertEquals проверяет, идентичны ли заданное значение и значение, полученное в результате работы метода.
@@ -9,11 +9,13 @@ public class LuckyTicketTestNg {
 
     @Test
     public void shouldCreateDefaultArrayTest() {
+        System.out.println("TEST1");
         Assert.assertEquals(arrayLuckyTicket.toString(), "[3, 8, 3, 3, 3, 8]");
         }
 
     @Test
     void verifyArrayEqualIntegerValue() {
+        System.out.println("TEST2");
         int[] array = {3, 8, 3, 3, 3, 8};
         int[] actualResult = LuckyTicket.resultArray(array);
 
@@ -21,4 +23,23 @@ public class LuckyTicketTestNg {
         Assert.assertEquals(expectedResult, actualResult);
     }
 
+    @BeforeMethod
+    public void beforeTest() {
+        System.out.println("This is text before each test");
     }
+
+    @AfterMethod
+    public void afterTest(){
+        System.out.println("This is text after each test");
+    }
+
+    @BeforeSuite
+    public void beforeAllTests() {
+        System.out.println("Before all tests …");
+    }
+
+    @AfterSuite
+    public void afterAllTests() {
+        System.out.println("After all tests …");
+    }
+}
